274ca5fcffa47d6f89a54d26b8968b3acd04ec04
rcn66 <rcorreanetto@ufl.edu>
2021-04-19 01:57:34 -0400

    Added Query 4
    Fully functional query 4 graphing
    
    -Can still improve on the formatting of the graph

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from time import strftime
 from flask import Flask, render_template, url_for, flash, redirect, request
-from forms import RegistrationForm, LoginForm, QueryOneForm, QueryThreeForm
+from forms import RegistrationForm, LoginForm, QueryOneForm, QueryThreeForm, QueryFourForm
 from flask_bootstrap import Bootstrap
 from datetime import datetime
 import cx_Oracle
 from DBconnection import connection
-from graphs import GraphOne, GraphThree
+from graphs import GraphOne, GraphThree, GraphFour
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -82,6 +82,31 @@ def queryThree():
 
     imgSrc = "src=static/defaultgraph.png"
     return render_template('QueryThree.html', title='Query Three', form=form, imgSrc=imgSrc)
+
+@app.route('/queryfour', methods=['GET', 'POST'])
+def queryFour():
+
+    form = QueryFourForm(request.form)
+
+    """ if form.validate():
+        counties = ', '.join(form.counties.data)
+        counties = '(' + counties + ')'
+        print(counties)
+
+        start_date = form.dStart.data.strftime('%m/%Y')
+        print(start_date)
+
+        end_date = form.dEnd.data.strftime('%m/%Y')
+        print(end_date) """
+
+
+    if request.method == 'POST' and form.validate():
+        imgSrc = GraphFour(request.form.getlist("counties"), request.form["dStart"], request.form["dEnd"], connection)
+        return render_template('QueryFour.html', title='Query Four', form=form, imgSrc=imgSrc)
+
+
+    imgSrc = "src=static/defaultgraph.png"
+    return render_template('QueryFour.html', title='Query Four', form=form, imgSrc=imgSrc)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- graphs.py
+++ graphs.py
@@ -117,3 +117,96 @@ def GraphThree(counties, start, end, threshold, connection):
         imgSrc = f"src=data:image/png;base64,{data}"
 
         return imgSrc
+
+def GraphFour(counties, start, end, connection):
+
+        whereClause = f"WHERE name IN ({str(counties).replace('[', '').replace(']','')})"
+
+        if start != '':
+            whereClause = whereClause + f" AND (year >= {str(start)[0:4]})"
+
+        if end != '':
+            whereClause = whereClause + f" AND (year <= {str(end)[0:4]})"
+
+        dbQuery = """
+        SELECT countyFIPS, name, year, (sum_AB / SQRT(sum_a2 * sum_b2)) AS correlation_coefficient  --Computes correlation for each year
+        FROM
+                (
+                SELECT countyFIPS, year, SUM(AB) AS sum_AB, SUM(a_sqr) AS sum_a2, SUM(b_sqr) AS sum_b2 --Sums up relevant values to compute correlation for each year
+                FROM
+                        (
+                        SELECT countyFIPS, year, month, a, b, (a * b) AS AB, POWER(a, 2) AS a_sqr, POWER(b, 2) AS b_sqr --Calculates relevant values for correlation equation
+                        FROM
+                                (
+                                SELECT countyFIPS, uv_year AS year, uv_month AS month, mean_uv, uv_value, (uv_value - mean_uv) AS a --Calculates difference of mean from value for correlation calculation
+                                FROM
+                                        (
+                                        SELECT countyFIPS, uv_year, AVG(uv_value) AS mean_UV --Groups by county and year, calculates average UV value for the summer (May - September)
+                                        FROM
+                                                (
+                                                SELECT countyFIPS, uv_year, uv_month, uv_value --For some reason cannot group in this SELECT statement? Selects summer months (May - September)
+                                                FROM rc4.Average_UV_Irradiance
+                                                WHERE uv_month >= 5 AND uv_month <= 9
+                                                )
+                                        GROUP BY countyFIPS, uv_year
+                                        )
+                                NATURAL JOIN
+                                        (SELECT * FROM rc4.Average_UV_Irradiance WHERE uv_month >= 5 AND uv_month <= 9) --Joins with UV Irradiance to get the monthly values to calculate difference with mean
+                                )  
+                        NATURAL JOIN
+                                (
+                                SELECT countyFIPS, year, month, mean_heat_year, month_average, (month_average - mean_heat_year) AS b --Calculates difference of mean from value for correlation calculation
+                                FROM
+                                        (
+                                        SELECT countyFIPS, year, AVG(heat_value) AS mean_heat_year --Calculates yearly heat averages
+                                        FROM (  SELECT t.*, EXTRACT(YEAR FROM HI_Date) AS year, TO_CHAR(HI_Date, 'mm') AS month --Extracts year and month
+                                                FROM rc4.Heat_Index t
+                                                )
+                                        WHERE year >= 2005
+                                        GROUP BY countyFIPS, year
+                                        )
+                                NATURAL JOIN
+                                        (
+                                        SELECT countyFIPS, year, month, AVG(heat_value) AS Month_Average --Calculates monthly averages
+                                        FROM (  SELECT t.*, EXTRACT(YEAR FROM HI_Date) AS year, TO_CHAR(HI_Date, 'mm') AS month --Extracts year and month
+                                                FROM rc4.Heat_Index t
+                                                )
+                                        WHERE year >= 2005
+                                        GROUP BY countyFIPS, year, month
+                                        )
+                                )
+                        )
+                GROUP BY countyFIPS, year
+                )
+        NATURAL JOIN
+                rc4.County
+        %s
+        ORDER BY countyFIPS ASC, year ASC
+        """ % (whereClause)
+
+        cursor = connection.cursor()
+
+        data = cursor.execute(dbQuery)  # Cursor.execute returns an iterator that contains the results of the query
+
+        x = []
+        y = []
+
+        for row in data:
+            x.append(row[2])
+            y.append(row[3])
+
+        plt.clf()
+        plt.plot(x, y)
+        plt.xlabel('Year')
+        plt.ylabel('Correlation Coefficient')
+        plt.tick_params(
+                axis='x',          # changes apply to the x-axis
+                labelbottom=False  # labels along the bottom edge are off
+        ) 
+
+        buf = BytesIO()
+        plt.savefig(buf, format="png")
+        data = base64.b64encode(buf.getbuffer()).decode("ascii")
+        imgSrc = f"src=data:image/png;base64,{data}"
+
+        return imgSrc
