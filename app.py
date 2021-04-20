40fb8a4812ec81e90e3f9434b809ca1c28272d32
Destin <destinholland@gmail.com>
2021-04-20 00:02:25 -0400

    query2

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from time import strftime
 from flask import Flask, render_template, url_for, flash, redirect, request
-from forms import RegistrationForm, LoginForm, QueryOneForm, QueryThreeForm, QueryFourForm
+from forms import RegistrationForm, LoginForm, QueryOneForm, QueryTwoForm, QueryThreeForm, QueryFourForm
 from flask_bootstrap import Bootstrap
 from datetime import datetime
 import cx_Oracle
 from DBconnection import connection
-from graphs import GraphOne, GraphThree, GraphFour
+from graphs import GraphOne, GraphTwo, GraphThree, GraphFour
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -57,6 +57,20 @@ def queryOne():
 
     imgSrc = "src=static/defaultgraph.png"
     return render_template('QueryOne.html', title='Query One', form=form, imgSrc=imgSrc)
+
+@app.route('/querytwo', methods=['GET', 'POST'])
+def queryTwo():
+
+    form = QueryTwoForm(request.form)
+
+    if request.method == 'POST' and form.validate():
+        print(request.form["county"])
+        imgSrc = GraphTwo(request.form["county"], request.form.getlist("ethnicities"), request.form["dStart"], request.form["dEnd"], connection)
+        return render_template('QueryTwo.html', title='Query Two', form=form, imgSrc=imgSrc)
+
+
+    imgSrc = "src=static/defaultgraph.png"
+    return render_template('QueryTwo.html', title='Query Two', form=form, imgSrc=imgSrc)
 
 @app.route('/querythree', methods=['GET', 'POST'])
 def queryThree():
--- graphs.py
+++ graphs.py
@@ -60,6 +60,89 @@ def GraphOne(counties, start, end, connection):
 
         return imgSrc
 
+def GraphTwo(county, ethnicities, start, end, connection):
+
+        outerWhereClause = f"WHERE countyFIPS == {county}"
+
+        ethnicities = str(ethnicities).replace('[', '').replace(']', '').replace("'", '')
+        outerWhereClause = outerWhereClause + f" AND (eID IN ({ethnicities})"
+
+        if start != '':
+                outerWhereClause = outerWhereClause + f" AND (year >= {str(start)[0:4]})"
+
+        if end != '':
+                outerWhereClause = outerWhereClause + f" AND (year <= {str(end)[0:4]})"
+
+        print(outerWhereClause)
+        dbQuery = """
+                SELECT d1.countyFIPS, startYear, endYear, eID, name AS ethnicity, percentChange --Joins with Ethnicity table to get the full names of the ethnicities
+                FROM(
+                    SELECT d1.countyFIPS, d1.year AS startYear, d2.year AS endYear, d1.ethnicity, (d2.ethnicPercent - d1.ethnicPercent) AS percentChange --Joins the two tables and calculates the difference(change) in ethnic percentage
+                    FROM
+                        (
+                        SELECT countyFIPS, year, ethnicity, (ethnicPop / totalPop) AS ethnicPercent --Calculates the percentage of total population for each ethnicity
+                        FROM(
+                            SELECT countyFIPS, demo_year AS year, ethnicity, SUM(population) as ethnicPop --Gets the population for each ethnicity for each county by year
+                            FROM Demographic
+                            GROUP BY countyFIPS, demo_year, ethnicity
+                            )
+                        NATURAL JOIN(
+                            SELECT countyFIPS, demo_year AS year, SUM(population) AS totalPop --Gets the total population for each county by year
+                            FROM Demographic
+                            GROUP BY countyFIPS, demo_year
+                            )
+                        ) d1
+                    JOIN
+                        (
+                        SELECT countyFIPS, year, ethnicity, (ethnicPop / totalPop) AS ethnicPercent --Calculates the percentage of total population for each ethnicity
+                        FROM(
+                            SELECT countyFIPS, demo_year AS year, ethnicity, SUM(population) as ethnicPop --Gets the population for each ethnicity for each county by year
+                            FROM Demographic
+                            GROUP BY countyFIPS, demo_year, ethnicity
+                            )
+                        NATURAL JOIN(
+                            SELECT countyFIPS, demo_year AS year, SUM(population) AS totalPop --Gets the total population for each county by year
+                            FROM Demographic
+                            GROUP BY countyFIPS, demo_year
+                            )
+                        ) d2
+                    ON (d1.countyFIPS = d2.countyFIPS) AND (d2.year = (d1.year + 1)) AND (d1.ethnicity = d2.ethnicity)
+                    )
+                JOIN
+                    Ethnicity
+                ON ethnicity = ethnicity.eid
+                %s;
+                --ORDER BY countyFIPS ASC, startYear ASC, eID ASC; For some reason, trying to order causes it to process forever(table probably too large)
+                        """ % (outerWhereClause)
+
+        cursor = connection.cursor()
+
+        data = cursor.execute(dbQuery)  # Cursor.execute returns an iterator that contains the results of the query
+
+        x = []
+        y = []
+
+        for row in data:
+                print(row)
+                # x.append(row[2] + str(row[1]))
+                # y.append(row[3])
+
+        plt.clf()
+        plt.plot(x, y)
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.tick_params(
+                axis='x',          # changes apply to the x-axis
+                labelbottom=False  # labels along the bottom edge are off
+        )
+
+        buf = BytesIO()
+        plt.savefig(buf, format="png")
+        data = base64.b64encode(buf.getbuffer()).decode("ascii")
+        imgSrc = f"src=data:image/png;base64,{data}"
+
+        return imgSrc
+
 def GraphThree(counties, start, end, threshold, connection):
 
         outerWhereClause = f"WHERE name IN ({str(counties).replace('[', '').replace(']','')})"
